1dffd715778e17ddebb8b79a50475b8683210e65
Ridho Taufikurahman <ridhotaufik2003@gmail.com>
2023-11-20 20:00:09 +0800

    di dalam stack_view saya menambahkan cara menggunakan Align, stack, Positioned

--- lib/module/stack/view/stack_view.dart
+++ lib/module/stack/view/stack_view.dart
@@ -8,6 +8,57 @@ class StackView extends StatefulWidget {
   Widget build(context, StackController controller) {
     controller.view = this;
     return Scaffold(
+        appBar: AppBar(
+          title: const Text("Dashboard"),
+          actions: const [],
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 160.0,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://images.unsplash.com/photo-1533050487297-09b450131914?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    8.0,
+                  ),
+                ),
+              ),
+              child: const Stack(
+                children: [
+                  Positioned(
+                    right: 10,
+                    bottom: 10,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+    return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -35,12 +86,12 @@ class StackView extends StatefulWidget {
               Positioned(
                 left: 20,
                 right: 20,
-                top: 50,
+                top: 100,
                 child: Container(
-                  height: 200,
+                  height: 120,
                   width: MediaQuery.of(context).size.width,
                   decoration: const BoxDecoration(
-                    color: Colors.yellow,
+                    color: Colors.white,
                     boxShadow: [
                       BoxShadow(
                         color: Color(0x19000000),
